4934a7e218767a5eb461d032a3ddd3b93eb2cec2
Jörg Viola <joerg@joergviola.de>
2012-06-12 14:28:56 +0200

    Heroku postgres

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+      "postgresql" % "postgresql" % "8.4-702.jdbc4",
       "com.github.twitter" % "bootstrap" % "2.0.2"
     )
 
