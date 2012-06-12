f9daa790ffc861062519e8a155662238104fd4e0
Jörg Viola <joerg@joergviola.de>
2012-06-12 08:32:42 +0200

    App is notified on publish

--- app/controllers/API.scala
+++ app/controllers/API.scala
@@ -2,11 +2,26 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object API extends Controller {
 
   def record(client: String) = Action {
     Ok(views.html.api.record.render(client)).as("text/javascript")
+  }
+
+  def published(client: String) = Action { implicit request =>
+    val form = Form(
+      tuple(
+        "title" -> text,
+        "page" -> text,
+        "category" -> text,
+        "description" -> text,
+        "id" -> text))
+    val (title, page, category, description, id) = form.bindFromRequest.get
+    println(client + " " + title + " " + page + " " + category + " " + description + " " + id)
+    Ok("")
   }
 
 }
