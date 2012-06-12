9334cf4fa859e01d8f7d20810ced1ab40689e882
Jörg Viola <joerg@joergviola.de>
2012-06-12 15:39:29 +0200

    Postgres timestamp

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,12 +5,12 @@
 CREATE TABLE Video (
   id			SERIAL PRIMARY KEY,
   client		VARCHAR(255) NOT NULL, 
-  date			DATETIME NOT NULL, 
+  date			TIMESTAMP NOT NULL, 
   title			VARCHAR(255) NOT NULL, 
   page			VARCHAR(255) NOT NULL, 
   category		VARCHAR(255) NOT NULL, 
   description	VARCHAR(255) NOT NULL, 
-  publishedId		VARCHAR(255) NOT NULL
+  publishedId	VARCHAR(255) NOT NULL
 );
  
 # --- !Downs
