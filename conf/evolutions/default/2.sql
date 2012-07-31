c91a3abe8e86d6f2def3d0262216fd8cf9b5ad50
Jörg Viola <joerg@joergviola.de>
2012-07-31 07:46:11 +0200

    evolution bug fixed

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -5,11 +5,14 @@
 CREATE TABLE Client (
   id			VARCHAR(255) NOT NULL, 
   created		TIMESTAMP NOT NULL, 
+  username		VARCHAR(255) NOT NULL, 
+  password		VARCHAR(255) NOT NULL,
+  initial_state	VARCHAR(255) NOT NULL,
   ytUser		VARCHAR(255) NOT NULL, 
-  ytPwd 		VARCHAR(255) NOT NULL, 
+  ytPwd 		VARCHAR(255) NOT NULL,
 );
 
-INSERT INTO Client VALUES ('videorec', SYSDATE(), 'joerg.viola@gmail.com', 'yDYf8PfWczURuKUPiuliUjPjZXO3uLIVP');
+INSERT INTO Client VALUES ('videorec', SYSDATE(), NULL, NULL, NULL, 'joerg.viola@gmail.com', 'yDYf8PfWczURuKUPiuliUjPjZXO3uLIVP');
  
 # --- !Downs
  
