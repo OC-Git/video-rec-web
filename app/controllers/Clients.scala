4cacfc2cfe9c47f14ba6e18e4059e29dc6933b68
Jörg Viola <joerg@joergviola.de>
2012-09-03 21:47:49 +0200

    First successful OAuth request

--- app/controllers/Clients.scala
+++ app/controllers/Clients.scala
@@ -5,25 +5,25 @@ import play.api._
 import play.api.libs.ws.WS
 
 object Clients extends Controller {
-  /*
+
   val ClientId = "307418826933.apps.googleusercontent.com"
   val ClientSecret = "6O6yaRcx7cgomDkLf1pHLijU"
 
   def auth(client: String) = Action { implicit request =>
     val q = "response_type=code" +
-      "client_id=" + ClientId +
-      "redirect_uri=" + routes.Clients.callback().absoluteURL() +
-      "scope=" +
-      "state=" + client +
-      "access_type=offline" +
-      "approval_prompt=auto"
+      "&client_id=" + ClientId +
+      "&redirect_uri=" + routes.Clients.callback().absoluteURL() +
+      "&scope=https://gdata.youtube.com" +
+      "&state=" + client +
+      "&access_type=offline" +
+      "&approval_prompt=auto"
 
     Redirect("https://accounts.google.com/o/oauth2/auth?" + q)
   }
   def callback = Action { implicit request =>
     val code = request.queryString("code")
     val client = request.queryString("state")
-    val params = Map("code" -> Seq(code),
+    val params = Map("code" -> code,
       "client_id" -> Seq(ClientId),
       "client_secret" -> Seq(ClientSecret),
       "redirect_uri" -> Seq(routes.Clients.callback().absoluteURL()),
@@ -34,9 +34,9 @@ object Clients extends Controller {
         val refreshToken = response.json \ "refresh_token"
         val expiresIn = response.json \ "expires_in"
         val tokenType = response.json \ "token_type"
-        Ok("")
+
+        Ok(accessToken)
       }
     }
   }
-*/
 }
