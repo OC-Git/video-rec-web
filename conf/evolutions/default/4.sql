3ee2df688e9c31b99509e32e8675e155af024412
Jörg Viola <joerg@joergviola.de>
2012-09-05 08:32:43 +0200

    fixed postgres sql

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -5,7 +5,7 @@ alter table Client add column usr varchar(255);
 alter table Client add column pwd varchar(255);
 alter table Client add column yt_token varchar(255);
 update Client set usr=ytUser, pwd=ytPwd;
-alter table Video alter publishedId varchar(255) null;	
+alter table Video alter column publishedId drop not null;
 
 # --- !Downs
 
